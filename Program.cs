03358c5200bf48a04bcc61a793ebb168719c0388
Daniel Walter <danielwalterpand@gmail.com>
2017-10-31 21:18:26 -0200

    Começo

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Tabela
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            String[] time = new string[5];
+
+            time[0] = "INTER";
+
+            Console.WriteLine(time[0]);
+
+        }
+    }
+}
