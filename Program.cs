aeb772d683f9ccc1894fa23f98d26f52cd18d0a0
Daniel Walter <danielwalterpand@gmail.com>
2017-10-31 21:39:55 -0200

    Implementação da pontuação (basic form)

--- Program.cs
+++ Program.cs
@@ -8,17 +8,25 @@ namespace Tabela
         {
 
             String[] time = new string[5];
+            int[] pontuação = new int[5];
 
-            time[0] = "Inter";
-            time[1] = "Grêmio";
-            time[2] = "Santos";
-            time[3] = "Criciúma";
-            time[4] = "Palmeiras";
+            pontuação[0] = 30;
+            time[0] = "Inter ";
+            pontuação[1] = 21;
+            time[1] = "Grêmio ";
+            pontuação[2] = 6;
+            time[2] = "Santos ";
+            pontuação[3] = 7;
+            time[3] = "Criciúma ";
+            pontuação[4] = 12;
+            time[4] = "Palmeiras ";
 
-            for(int i = 0; i < time.Length; i++)
-            {
-                Console.WriteLine(time[i]);
-            }
+                for(int i = 0; i < time.Length; i++)
+                {
+                    Console.WriteLine("Time: " + time[i] + "Pontuação: " + pontuação[i]);
+                }
+
+            
 
         }
     }
